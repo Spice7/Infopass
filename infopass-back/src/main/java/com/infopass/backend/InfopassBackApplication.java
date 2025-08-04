7dbe63ded45157a9b408289c88f4abbc24ef4d1a
LKH <airforce940706@gmail.com>
2025-08-04 10:41:27 +0900

    [이건호] 최초 프로젝트 커밋

---
+++ infopass-back/src/main/java/com/infopass/backend/InfopassBackApplication.java
@@ -0,0 +1,13 @@
+package com.infopass.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfopassBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfopassBackApplication.class, args);
+	}
+
+}
