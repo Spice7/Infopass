e73609464e2d519cca7145063c10ac548cfd2b62
kkb0329 <kkb0329@gmail.com>
2025-08-05 09:58:59 +0900

    first commit test

--- infopass-front/src/router/RouterMain.jsx
+++ infopass-front/src/router/RouterMain.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { Route, Router, Routes } from 'react-router-dom'
 import AdminRoutes from './AdminRoutes'
 import OxQuizRoutes from './OxQuizRoutes'
+import Home from './Home'
 
 const RouterMain = () => {
     return (
         <div>
             <Routes>
+                <Route path="/" element={<Home />} />
                 {AdminRoutes}
                 {OxQuizRoutes}
-                <Route path='*' element={<h1>잘못된 주소입니다</h1>} />
+                <Route path="*" element={<h1>잘못된 주소입니다</h1>} />
             </Routes>
         </div>
     )
