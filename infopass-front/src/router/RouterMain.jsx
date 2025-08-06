9dd2d0d40c03e0d5fa1d7512cbca36786e330872
LKH <airforce940706@gmail.com>
2025-08-06 09:13:30 +0900

    [이건호] 블록게임 로직 구현중 / 백엔드 분할

--- infopass-front/src/router/RouterMain.jsx
+++ infopass-front/src/router/RouterMain.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Router, Routes } from 'react-router-dom'
 import AdminRoutes from './AdminRoutes'
 import OxQuizRoutes from './OxQuizRoutes'
+import BlockRoutes from './BlockRoutes'
 
 const RouterMain = () => {
     return (
@@ -9,6 +10,7 @@ const RouterMain = () => {
             <Routes>
                 {AdminRoutes}
                 {OxQuizRoutes}
+                {BlockRoutes}
                 <Route path='*' element={<h1>잘못된 주소입니다</h1>} />
             </Routes>
         </div>
