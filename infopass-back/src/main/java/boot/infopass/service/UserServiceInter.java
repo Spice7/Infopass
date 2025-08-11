89fde2eb16c2d5a34464c604cf5e2a001da71d9a
LKH <airforce940706@gmail.com>
2025-08-11 10:10:55 +0900

    08.11 dev 최종 병합

--- infopass-back/src/main/java/boot/infopass/service/UserServiceInter.java
+++ infopass-back/src/main/java/boot/infopass/service/UserServiceInter.java
@@ -5,11 +5,7 @@ public interface UserServiceInter {
 	public int insertUser(UserDto userDto);
 	public UserDto getUserData(Integer id);
 	public boolean findById(String email);
-<<<<<<< HEAD
-	
-	
+
 	public UserDto updateUser(Long id, UserDto updatedUserDto);
-=======
 	public boolean findByNickName(String nickname);
->>>>>>> origin/jung
 }
